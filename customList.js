a06b397ae857868ec489d434d2288dbd53e9c1db
Kiryl Kalasouski <kirylk>
2015-10-09 16:55:37 +0300

    no message

--- customList.js
+++ customList.js
@@ -1,13 +1,19 @@
-function CustomList(elemId){
-    BaseList.apply(this, arguments);
+window.CustomList = window.CustomList || (function(undefined){
+	
+	return function(elemId){
+	
+		var self = this;
+		BaseList.apply(self, arguments);
+	
+		self.sort= function() {
+			self._collection.sort(function(a, b){
+				if(a.text < b.text) return -1;
+				if(a.text > b.text) return 1;
+				return 0;
+			});
 
-    this.sort= function() {
-        this._collection.sort(function(a, b){
-            if(a.text < b.text) return -1;
-            if(a.text > b.text) return 1;
-            return 0;
-        });
-
-        this.render(true);
-    }
-}
+			self.render(true);
+		};
+	};
+	
+})();
